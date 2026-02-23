dff896182edf9d355f13fc3523b2b77db1268ed9
Marcelo Zanguettin Pereira <mzanguettin@gmail.com>
2026-02-23 13:37:32 -0300

    Correção

--- backend/schemas/pedido.py
+++ backend/schemas/pedido.py
@@ -6,7 +6,6 @@ from .produto import Produto
 class ItemPedido(BaseModel):
     produto_id: int
     quantidade: int = Field(..., gt=0) # Quantidade deve ser maior que 0
-    preco_unitario: float
 
 class Pedido(BaseModel):
     id: Optional[int] = None
@@ -15,3 +14,17 @@ class Pedido(BaseModel):
     itens: list[ItemPedido]
     total: float
     status: str = "Pendente" # Pendente, Pago, Cancelado
+
+class ItemPedidoResponse(BaseModel):
+    id: int
+    produto_id: int
+    quantidade: int
+    preco_unitario: float
+
+class PedidoResponse(BaseModel):
+    id: int
+    cliente_id: int
+    data: datetime
+    itens: list[ItemPedidoResponse]
+    total: float
+    status: str
